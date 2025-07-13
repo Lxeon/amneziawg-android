ce2aacc9c4503fff4785fe95d93a9e412dfb1702
Lxeon <1277454443@qq.com>
2025-07-13 13:53:42 +0800

    optimized code; allow recursive resolver; fix srv dns identifying Errors

--- tunnel/src/main/java/org/amnezia/awg/config/InetEndpoint.java
+++ tunnel/src/main/java/org/amnezia/awg/config/InetEndpoint.java
@@ -7,6 +7,8 @@ package org.amnezia.awg.config;
 import android.util.Log;
 import androidx.annotation.Nullable;
 import org.amnezia.awg.util.NonNullForAll;
+import org.xbill.DNS.AAAARecord;
+import org.xbill.DNS.ARecord;
 import org.xbill.DNS.DohResolver;
 import org.xbill.DNS.ExtendedResolver;
 import org.xbill.DNS.Lookup;
@@ -17,11 +19,9 @@ import org.xbill.DNS.TXTRecord;
 import org.xbill.DNS.TextParseException;
 import org.xbill.DNS.Type;
 
-import java.net.Inet4Address;
 import java.net.Inet6Address;
 import java.net.InetAddress;
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.UnknownHostException;
 import java.time.Duration;
 import java.time.Instant;
@@ -38,6 +38,9 @@ import java.util.regex.Pattern;
 @NonNullForAll
 public final class InetEndpoint {
     private static final Pattern BARE_IPV6 = Pattern.compile("^[^\\[\\]]*:[^\\[\\]]*");
+    private static final Pattern SRV_PATTERN = Pattern.compile(
+            "^_[a-zA-Z0-9-]+\\._(tcp|udp|\\w+)\\.[a-zA-Z0-9-]+(\\.[a-zA-Z0-9-]+)*\\.[a-zA-Z]{2,}\\.?$"
+    );
     private static final Pattern FORBIDDEN_CHARACTERS = Pattern.compile("[/?#]");
     private static final String TAG = "AmneziaWG/InetEndpoint";
 
@@ -45,6 +48,7 @@ public final class InetEndpoint {
     private final boolean isResolved;
     private final Object lock = new Object();
     private final int port;
+    private final int resolveTimes = 5;
     private Instant lastResolution = Instant.EPOCH;
     @Nullable
     private InetEndpoint resolved;
@@ -55,14 +59,33 @@ public final class InetEndpoint {
         this.port = port;
     }
 
+    int resolveCount = 0;
+
     public static InetEndpoint parse(final String endpoint) throws ParseException {
         if (FORBIDDEN_CHARACTERS.matcher(endpoint).find())
             throw new ParseException(InetEndpoint.class, endpoint, "Forbidden characters");
 
-        final URI uri;
+        URI uri = null;
         try {
-            uri = new URI("awg://" + endpoint);
-        } catch (final URISyntaxException e) {
+
+            // check srv domain host, _wireguard._udp.example.com
+            String[] parts = endpoint.split(":");
+            if (parts.length == 2 && isSrvDomain(parts[0])) {
+                InetEndpoint t = new InetEndpoint("", false, Integer.parseInt(parts[1]));
+
+                Optional<InetEndpoint> opt = t.startResolve(parts[0]);
+                if (opt.isEmpty()) {
+                    throw new ParseException(InetEndpoint.class, endpoint, "Invalid srv domain");
+                }
+                t = opt.get();
+                uri = new URI("awg://" + t.getHost() + ":" + t.getPort());
+            }
+
+            if (uri == null) {
+                uri = new URI("awg://" + endpoint);
+
+            }
+        } catch (final Exception e) {
             throw new ParseException(InetEndpoint.class, endpoint, e);
         }
 
@@ -77,6 +100,42 @@ public final class InetEndpoint {
             // Failed to parse the host as a numeric address, so it must be a DNS hostname/FQDN.
             return new InetEndpoint(uri.getHost(), false, uri.getPort());
         }
+    }
+
+    public static boolean isSrvDomain(String domain) {
+        domain = domain.trim();
+        if (domain.isEmpty()) {
+            return false;
+        }
+        return SRV_PATTERN.matcher(domain).matches();
+    }
+
+    private static Lookup getLookup(String domain, int type) {
+        Lookup lookup = null;
+        try {
+            lookup = new Lookup(domain, type);
+        } catch (TextParseException e) {
+            throw new RuntimeException(e);
+        }
+        lookup.setCache(null);
+        Resolver[] resolvers = new Resolver[2];
+        resolvers[0] = new DohResolver("https://223.5.5.5/dns-query");
+        resolvers[1] = new DohResolver("https://doh.360.cn/dns-query");
+        ExtendedResolver exResolver = new ExtendedResolver(resolvers);
+        lookup.setResolver(exResolver);
+        return lookup;
+    }
+
+    private Optional<InetEndpoint> resolveIp4p(InetAddress address) throws UnknownHostException {
+        if (address instanceof Inet6Address) {
+            byte[] v6 = address.getAddress();
+            if ((v6[0] == 0x20) && (v6[1] == 0x01) && (v6[2] == 0x00) && (v6[3] == 0x00)) {
+                InetAddress v4 = InetAddress.getByAddress(Arrays.copyOfRange(v6, 12, 16));
+                int p = ((v6[10] & 0xFF) << 8) | (v6[11] & 0xFF);
+                return Optional.of(new InetEndpoint(Objects.requireNonNull(v4.getHostAddress()), true, p));
+            }
+        }
+        return Optional.empty();
     }
 
     /**
@@ -95,102 +154,134 @@ public final class InetEndpoint {
         synchronized (lock) {
             if (Duration.between(lastResolution, Instant.now()).toMinutes() > 1) {
                 // 优先顺序：SRV -> TXT -> IP4P -> 普通DNS解析
-                Optional<InetEndpoint> resolvedOpt;
-
-                // 1. SRV
-                resolvedOpt = resolveViaSrv(host);
-                if (resolvedOpt.isPresent()) {
-                    Log.i(TAG, "[DNS] SRV record find, wait for next resolve");
-                    resolved = resolvedOpt.get();
-                    return resolvedOpt;
-                }
-
-                // 2. TXT
-                resolvedOpt = resolveViaTxt(host);
-                if (resolvedOpt.isPresent()) {
-                    Log.i(TAG, "[DNS] TXT record find, wait for next resolve");
-                    resolved = resolvedOpt.get();
-                    return resolvedOpt;
-                }
-
-                try {
-                    InetAddress[] candidates = InetAddress.getAllByName(host);
-                    InetAddress selected = candidates[0];
-
-                    for (InetAddress addr : candidates) {
-                        if (addr instanceof Inet4Address) {
-                            selected = addr;
-                            break;
-                        }
-                    }
-
-                    // 3. IP4P
-                    Optional<InetEndpoint> resolvedIp4p = resolveIp4p(selected);
-                    if (resolvedIp4p.isPresent()) {
-                        Log.i(TAG, "[DNS] ip4p record find");
-                        resolved = resolvedIp4p.get();
-                        lastResolution = Instant.now();
-                        return resolvedIp4p;
-                    }
-
-                    // 4. ipv4/ipv6
-                    if (resolved == null && selected.getHostAddress() != null) {
-                        resolved = new InetEndpoint(selected.getHostAddress(), true, port);
-                    }
-
-                    lastResolution = Instant.now();
-                } catch (UnknownHostException ignored) {
-                    resolved = null;
-                }
+                return startResolve(host);
             }
 
             return Optional.ofNullable(resolved);
         }
     }
 
-    private Optional<InetEndpoint> resolveIp4p(InetAddress address) throws UnknownHostException {
-        if (address instanceof Inet6Address) {
-            byte[] v6 = address.getAddress();
-            if ((v6[0] == 0x20) && (v6[1] == 0x01) && (v6[2] == 0x00) && (v6[3] == 0x00)) {
-                InetAddress v4 = InetAddress.getByAddress(Arrays.copyOfRange(v6, 12, 16));
-                int p = ((v6[10] & 0xFF) << 8) | (v6[11] & 0xFF);
-                return Optional.of(new InetEndpoint(Objects.requireNonNull(v4.getHostAddress()), true, p));
+    private Optional<InetEndpoint> startResolve(String host) {
+        int port = this.port;
+        if (resolved != null) {
+            port = resolved.port;
+        }
+        resolveCount++;
+        if (!isResolved && resolveCount > resolveTimes) {
+            Log.w(TAG, "[DNS] Resolving " + host + " too many times");
+            return Optional.empty();
+        }
+        // 优先顺序：SRV -> TXT -> IP4P -> 普通DNS解析
+        Optional<InetEndpoint> opt;
+
+        // 1. SRV
+        opt = resolveSrv(host);
+        if (opt.isPresent()) {
+            Log.i(TAG, "[DNS] SRV record find, wait for next resolve");
+            resolved = opt.get();
+        }
+        if (resolved != null) {
+            host = resolved.host;
+            port = resolved.port;
+        }
+
+        // 2. TXT
+        opt = resolveTxt(host);
+        if (opt.isPresent()) {
+            Log.i(TAG, "[DNS] TXT record find, wait for next resolve");
+            resolved = opt.get();
+            if (resolved.isResolved) {
+                return Optional.of(resolved);
             }
         }
-        return Optional.empty();
-    }
+        if (resolved != null) {
+            host = resolved.host;
+            port = resolved.port;
+        }
 
-    private Optional<InetEndpoint> resolveViaSrv(String domain) {
+        // 先不急着返回，看看是不是还有域名
         try {
-            Lookup lookup = getLookup(domain, Type.SRV);
-            Record[] records = lookup.run();
 
-            if (records == null || records.length == 0) {
-                return Optional.empty();
+            // ipv4
+            Optional<InetAddress> optIp = resolveIpv4(host);
+
+            if (optIp.isPresent()) {
+                InetAddress addr = optIp.get();
+                resolved = new InetEndpoint(Objects.requireNonNull(addr.getHostAddress()), true, port);
+                lastResolution = Instant.now();
+                return Optional.of(resolved);
             }
 
-            SRVRecord srv = (SRVRecord) records[0];
-            String targetHost = srv.getTarget().toString(true); // 去掉尾部的点
-            int port = srv.getPort();
-            return Optional.of(new InetEndpoint(targetHost, false, port));
-        } catch (TextParseException | UnknownHostException e) {
-            Log.e(TAG, Log.getStackTraceString(e));
+            optIp = resolveIpv6(host);
+            if (optIp.isPresent()) {
+                InetAddress addr = optIp.get();
+
+                // 3. IP4P
+                opt = resolveIp4p(addr);
+                if (opt.isPresent()) {
+                    Log.i(TAG, "[DNS] ip4p record find");
+                    resolved = opt.get();
+                    lastResolution = Instant.now();
+                    return Optional.of(resolved);
+                }
+
+                resolved = new InetEndpoint(Objects.requireNonNull(addr.getHostAddress()), true, port);
+                lastResolution = Instant.now();
+                return Optional.of(resolved);
+            }
+
+            if (!resolved.isResolved) {
+                // 递归查询，srv和txt记录仍然可能是域名
+                startResolve(resolved.host);
+            }
+
+        } catch (UnknownHostException ignored) {
+            resolved = null;
         }
-        return Optional.empty();
+        return Optional.ofNullable(resolved);
     }
 
-    private Lookup getLookup(String domain, int type) throws TextParseException, UnknownHostException {
-        Lookup lookup = new Lookup(domain, type);
-        lookup.setCache(null);
-        Resolver[] resolvers = new Resolver[2];
-        resolvers[0] = new DohResolver("https://223.5.5.5/dns-query");
-        resolvers[1] = new DohResolver("https://doh.360.cn/dns-query");
-        ExtendedResolver exResolver = new ExtendedResolver(resolvers);
-        lookup.setResolver(exResolver);
-        return lookup;
+    private Optional<InetAddress> resolveIpv4(String domain) {
+        Lookup lookup = getLookup(domain, Type.A);
+        Record[] records = lookup.run();
+
+        if (records == null || records.length == 0) {
+            return Optional.empty();
+        }
+
+        ARecord a = (ARecord) records[0];
+        InetAddress target = a.getAddress();
+        return Optional.of(target);
     }
 
-    private Optional<InetEndpoint> resolveViaTxt(String domain) {
+    private Optional<InetAddress> resolveIpv6(String domain) {
+        Lookup lookup = getLookup(domain, Type.AAAA);
+        Record[] records = lookup.run();
+
+        if (records == null || records.length == 0) {
+            return Optional.empty();
+        }
+
+        AAAARecord a = (AAAARecord) records[0];
+        InetAddress target = a.getAddress();
+        return Optional.of(target);
+    }
+
+    private Optional<InetEndpoint> resolveSrv(String domain) {
+        Lookup lookup = getLookup(domain, Type.SRV);
+        Record[] records = lookup.run();
+
+        if (records == null || records.length == 0) {
+            return Optional.empty();
+        }
+
+        SRVRecord srv = (SRVRecord) records[0];
+        String targetHost = srv.getTarget().toString(true); // 去掉尾部的点
+        int port = srv.getPort();
+        return Optional.of(new InetEndpoint(targetHost, false, port));
+    }
+
+    private Optional<InetEndpoint> resolveTxt(String domain) {
         try {
             Lookup lookup = getLookup(domain, Type.TXT);
             Record[] records = lookup.run();
@@ -207,7 +298,7 @@ public final class InetEndpoint {
                     }
                 }
             }
-        } catch (TextParseException | NumberFormatException | UnknownHostException e) {
+        } catch (NumberFormatException e) {
             Log.e(TAG, Log.getStackTraceString(e));
         }
         return Optional.empty();
--- tunnel/src/test/java/org/amnezia/awg/config/InetEndPointTest.java
+++ tunnel/src/test/java/org/amnezia/awg/config/InetEndPointTest.java
@@ -7,8 +7,12 @@ import java.util.Optional;
 public class InetEndPointTest {
 
     @Test
-    public void resolveViaSrv() throws ParseException {
-        InetEndpoint inetEndpoint = InetEndpoint.parse("text.example.com:51820");
+    public void parse() throws ParseException {
+        parse("_wireguard._udp.example.top:51820");
+    }
+
+    public void parse(String endpoint) throws ParseException {
+        InetEndpoint inetEndpoint = InetEndpoint.parse(endpoint);
         Optional<InetEndpoint> resolved = inetEndpoint.getResolved();
         resolved.ifPresent(System.out::println);
     }
